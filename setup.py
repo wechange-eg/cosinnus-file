e583e2c14eaf83fae6a138e5fb36f61219fadb15
Markus Holtermann <info@markusholtermann.eu>
2013-12-02 16:05:57 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir != '':
     os.chdir(root_dir)
-django_dir = 'cosinnus'
+pkgdir = 'cosinnus_file'
 
-for dirpath, dirnames, filenames in os.walk(django_dir):
+for dirpath, dirnames, filenames in os.walk(pkgdir):
     # Ignore PEP 3147 cache dirs and those whose names start with '.'
     dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
     if '__init__.py' in filenames:
@@ -40,25 +40,20 @@ for dirpath, dirnames, filenames in os.walk(django_dir):
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
+from cosinnus_file import get_version
 
 setup(
     name='cosinnus-file',
-    version='0.1a0',
+    version=get_version(),
     description='cosinnus file upload, storage and organisation application',
     long_description=read('README'),
     author='Sinnwerkstatt Medienagentur GmbH Berlin',
-    author_email='web@Sinnwerkstatt.com',
+    author_email='web@sinnwerkstatt.com',
     packages=find_packages(exclude=["tests"]),
     data_files=data_files,
     install_requires=[
-        'Django>=1.5',
-        'South>=0.7',
-        'django-appconf>=0.6',
-        'django-taggit>=0.10a1',
+        'cosinnus>=0.1dev',
     ],
-    extras_require = {
-        'bootstrap':  ['django-bootstrap-toolkit>=2.11.5'],
-    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
