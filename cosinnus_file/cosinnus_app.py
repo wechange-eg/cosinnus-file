9584a4c2f84b1b9628312f850e8033ec48e27957
Sascha Narr <saschanarr@googlemail.com>
2013-12-17 12:46:46 +0100

    Fixed FZS#9: App-Beschriftungen übersetzen

--- cosinnus_file/cosinnus_app.py
+++ cosinnus_file/cosinnus_app.py
@@ -4,6 +4,8 @@ from cosinnus_file.utils import renderer
 
 IS_COSINNUS_APP = True
 COSINNUS_APP_NAME = 'file'
+COSINNUS_APP_LABEL = 'Files'
+
 
 ATTACHABLE_OBJECT_MODELS = ['cosinnus_file.FileEntry']
 ATTACHABLE_OBJECT_RENDERERS = {'cosinnus_file.FileEntry': renderer.FileEntryRenderer}
