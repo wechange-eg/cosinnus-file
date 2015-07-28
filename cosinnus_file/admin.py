0797bd7f582d367b3c9d58afa5ab8aff5f03570d
Sascha Narr <saschanarr@googlemail.com>
2015-07-28 14:23:14 +0200

    Nicer file admin

--- cosinnus_file/admin.py
+++ cosinnus_file/admin.py
@@ -7,7 +7,8 @@ from cosinnus_file.models import FileEntry
 
 
 class FileEntryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'path', 'file', 'group', 'created', 'creator')
+    list_display = ('title', 'is_container', 'path', 'file', 'group', 'created', 'creator')
+    list_filter = ('group', )
     search_fields = ('title', 'note')
 
 
