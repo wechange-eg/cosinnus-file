35352277001096ae42ed024f4f083455527ce36a
Sascha Narr <saschanarr@googlemail.com>
2015-07-29 16:24:42 +0200

    PDF downloads open within the browser

--- cosinnus_file/views.py
+++ cosinnus_file/views.py
@@ -282,19 +282,21 @@ class FileDownloadView(RequireReadMixin, FilterGroupMixin, View):
             response['Content-Length'] = fileentry._filesize
             if encoding is not None:
                 response['Content-Encoding'] = encoding
-        
-            # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
-            if u'WebKit' in request.META['HTTP_USER_AGENT']:
-                # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-                filename_header = 'filename=%s' % clean_filename(filename)
-            elif u'MSIE' in request.META['HTTP_USER_AGENT']:
-                # IE does not support internationalized filename at all.
-                # It can only recognize internationalized URL, so we do the trick via routing rules.
-                filename_header = ''
-            else:
-                # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-                filename_header = 'filename*=UTF-8\'\'%s' % clean_filename(filename)
-            response['Content-Disposition'] = 'attachment; ' + filename_header
+
+                
+            if content_type not in settings.COSINNUS_FILE_NON_DOWNLOAD_MIMETYPES:
+                # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
+                if u'WebKit' in request.META['HTTP_USER_AGENT']:
+                    # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
+                    filename_header = 'filename=%s' % clean_filename(filename)
+                elif u'MSIE' in request.META['HTTP_USER_AGENT']:
+                    # IE does not support internationalized filename at all.
+                    # It can only recognize internationalized URL, so we do the trick via routing rules.
+                    filename_header = ''
+                else:
+                    # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
+                    filename_header = 'filename*=UTF-8\'\'%s' % clean_filename(filename)
+                response['Content-Disposition'] = 'attachment; ' + filename_header
         
         return response
     
