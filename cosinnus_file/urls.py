8ef0e74e7bd0fe1f5bc7f4566629ab3e61fbcc3c
Sascha Narr <saschanarr@googlemail.com>
2018-09-26 18:58:18 +0200

    django1.9 prep - URLs and imports

--- cosinnus_file/urls.py
+++ cosinnus_file/urls.py
@@ -1,31 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from cosinnus_file import views
+
+cosinnus_root_patterns = []
 
 
-cosinnus_root_patterns = patterns('', )
+cosinnus_group_patterns = [
+    url(r'^list/$', views.file_hybrid_list_view, name='list'),
+    url(r'^list/download/$', views.folder_download_view, name='download-folder'),
+    url(r'^list/move_element/$', views.move_element_view, name='move-element'),
+    url(r'^list/(?P<slug>[^/]+)/$', views.file_hybrid_list_view, name='list'),
+    url(r'^list/(?P<slug>[^/]+)/download/$', views.folder_download_view, name='download-folder'),
+    url(r'^upload_inline/$', views.file_upload_inline, name='upload-inline'),
+    url(r'^(?P<slug>[^/]+)/$', views.file_update_view, name='edit'),
 
-
-cosinnus_group_patterns = patterns('cosinnus_file.views',
-    #url(r'^list/$',
-    #    'file_list_view',
-    #    name='list'),
-    url(r'^list/$', 'file_hybrid_list_view', name='list'),
-    url(r'^list/download/$', 'folder_download_view', name='download-folder'),
-    url(r'^list/move_element/$', 'move_element_view', name='move-element'),
-    url(r'^list/(?P<slug>[^/]+)/$', 'file_hybrid_list_view', name='list'),
-    url(r'^list/(?P<slug>[^/]+)/download/$', 'folder_download_view', name='download-folder'),
-    url(r'^upload_inline/$', 'file_upload_inline', name='upload-inline'),
-    url(r'^(?P<slug>[^/]+)/$', 'file_update_view', name='edit'),
-
-    #url(r'^list/(?P<tag>[^/]+)/$', 'file_list_view', name='list-filtered'),
-    #url(r'^(?P<slug>[^/]+)/$', 'file_detail_view', name='file'),
-    url(r'^(?P<slug>[^/]+)/download$', 'file_download_view', name='download'),
-    url(r'^(?P<slug>[^/]+)/save', 'file_download_view', {'force_download': True}, name='save'),
-    url(r'^(?P<slug>[^/]+)/delete/$', 'file_delete_view', {'form_view': 'delete'}, name='delete'),
+    #url(r'^list/(?P<tag>[^/]+)/$', views.file_list_view', name='list-filtered'),
+    #url(r'^(?P<slug>[^/]+)/$', views.file_detail_view', name='file'),
+    url(r'^(?P<slug>[^/]+)/download$', views.file_download_view, name='download'),
+    url(r'^(?P<slug>[^/]+)/save', views.file_download_view, {'force_download': True}, name='save'),
+    url(r'^(?P<slug>[^/]+)/delete/$', views.file_delete_view, {'form_view': 'delete'}, name='delete'),
     
-    url(r'^$', 'file_index_view', name='index'),
-)
+    url(r'^$', views.file_index_view, name='index'),
+]
 
 urlpatterns = cosinnus_group_patterns + cosinnus_root_patterns
